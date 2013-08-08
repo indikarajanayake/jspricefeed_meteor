03da5c916b31aef30f3824c5b90a9948a33e6cca
indikarajanayake <indikarajanayake@gmail.com>
2013-08-08 19:34:28 +1000

    Add publish and subscribes

--- client/exchanges.js
+++ client/exchanges.js
@@ -1,7 +1,9 @@
+//Exchange collection in client side
 Exchanges	= new Meteor.Collection("exchanges");
 
+//populate templates
 Template.exchangesTemplate.exchanges	= function(){
-	Exchanges.remove();	
+	
 	return Exchanges.find();
 };
 
@@ -9,7 +11,7 @@ Template.exchangeTemplate.exchange	= function(){
 	return Exchanges.findOne({code:Session.get('currentExchangeId')});
 };
 
-
+//routers
 Meteor.Router.add({
 	'/':'exchangesTemplate',
 	'/exchange/:id': function(id){
@@ -17,5 +19,11 @@ Meteor.Router.add({
 				return 'exchangeTemplate'
 			}
 });
+//wrap with autosubscribe to trigger when session changes
+Meteor.autosubscribe(function (){
+//get selected exchange details
+Meteor.subscribe("exchangeDetail", Session.get('currentExchangeId'));
+});
 
-
+// get details of exchanges
+Meteor.subscribe("exchangesDetails");
--- server/exchanges.js
+++ server/exchanges.js
@@ -1,15 +1,28 @@
+//Collection to store Exchanges 
 Exchanges	= new  Meteor.Collection("exchanges");
-
+//start up funciton add sample exchange 
 Meteor.startup(function(){
 
 	if(Exchanges.find().count() == 0){
 		Exchanges.insert({code:"DFM",symbols:[{code:"DFM",ltp:'10',ltq:'20'}]});
 	}
 });
-
+//mock function to update the exchange documents 
 
 Meteor.setInterval(function(){
 Exchanges.update({code:"DFM", "symbols.code":"DFM"},{$inc :{"symbols.$.ltp":1}},false, true);
 
 
 },1000);
+
+// publish selected exchange
+
+Meteor.publish("exchangeDetail", function(currentExchangeId){
+
+	return Exchanges.findOne({code:currentExchangeId});
+
+});
+//publish exchanges details. Initally send all the data 
+Meteor.publish("exchangesDetails", function(){
+	return Exchanges.find();
+});
